34c0312442da27dbaa65da56cb6960716c0748f9
Mohamed Boudjatat <mboudjatat17@hotmail.com>
2023-02-14 16:21:40 +0000

    Automated

--- environment/environment/spec-tests/app/provision.sh
+++ environment/environment/spec-tests/app/provision.sh
@@ -10,6 +10,14 @@ sudo apt-get install nginx -y
 # Enable/Start nginx
 sudo systemctl enable nginx -y
 
+# Reverse proxy setup:
+# Remove default nginx configuration
+sudo rm /etc/nginx/sites-available/default
+# Copy the reverse proxy file containing the relevant code into that location
+sudo cp environment/environment/spec-tests/app/reverse_proxy.txt /etc/nginx/sites-available/default.conf
+# Restart nginx to confirm changes
+sudo systemctl restart nginx -y
+
 # Install node js specific version
 sudo apt-get install python-software-properties
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
@@ -18,10 +26,15 @@ sudo apt-get install nodejs -y
 # Install pm2
 sudo npm install pm2 -g
 
-# Reverse proxy setup:
-# Remove default nginx configuration
-sudo rm /etc/nginx/sites-available/default
-# Copy the reverse proxy file containing the relevant code into that location
-sudo cp app/reverse_proxy /etc/nginx/sites-available/default
-# Restart nginx to confirm changes
-sudo systemctl restart nginx -y
+echo 'export DB_HOST=mongodb://192.168.10.150:27017/posts' >> ~/.bashrc 
+source .bashrc
+
+# Install app
+cd app
+cd app
+npm install
+# Seed the Database
+node seeds/seed.js
+
+
+
